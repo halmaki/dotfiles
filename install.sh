d9bdc572048f13510f8495fa76b86fe46d1ef5e0
ichikawa koki <chicken00260@gmail.com>
2025-02-02 02:09:40 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,28 +2,44 @@
 
 set -u
 
-BASEDIR=$(dirname $0)
-DOTPATH=$PWD
-cd $BASEDIR
+DOTPATH=$(cd "$(dirname "$0")" && pwd)
+cd "$DOTPATH"
 
-echo $BASEDIR
-echo $DOTPATH
-for f in .??*; do
-  [ "$f" = ".git" ] && continue
-  [ "$f" = ".config" ] && continue
-  ln -snfv $DOTPATH/"$f" ~/
-done
+link_dotfiles() {
+  for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".config" ] && continue
+    [ "$f" = ".Xresources" ] && continue
+    ln -snfv $DOTPATH/"$f" ~/
+  done
 
-ln -snfv $DOTPATH/.config/nvim ~/.config/
+  if [ "$(uname)" = "Linux" ]; then
+    ln -snfv "$DOTFILES_DIR/.Xresources" ~/
+  fi
+}
 
-if [ "$(uname)" == 'Darwin' ]; then
+link_config_files() {
 
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  ln -snfv $DOTPATH/.config/dunst ~/.config/
-  ln -snfv $DOTPATH/.config/i3 ~/.config/
-  ln -snfv $DOTPATH/.config/picom ~/.config/
-  ln -snfv $DOTPATH/.config/rofi ~/.config/
-else
-  echo "Your platform ($(uname -a)) is not supported."
-  exit 1
-fi
+  echo "Apply common config"
+  ln -snfv $DOTPATH/.config/nvim ~/.config/
+
+  case "$(uname)" in
+  Darwin)
+    echo "Apply config for macOS"
+    ;;
+  Linux)
+    echo "Apply config for Linux"
+    ln -snfv "$DOTPATH/.config/dunst" ~/.config/
+    ln -snfv "$DOTPATH/.config/i3" ~/.config/
+    ln -snfv "$DOTPATH/.config/picom" ~/.config/
+    ln -snfv "$DOTPATH/.config/rofi" ~/.config/
+    ;;
+  *)
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+    ;;
+  esac
+}
+
+link_dotfiles
+link_config_files
