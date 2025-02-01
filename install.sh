16786d0820113dc759f853b010ac52ebe73804c1
ichikawa koki <chicken00260@gmail.com>
2025-02-02 01:27:52 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,21 @@ cd $BASEDIR
 echo $BASEDIR
 echo $DOTPATH
 for f in .??*; do
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".config" ] && continue
-    ln -snfv $DOTPATH/"$f" ~/
+  [ "$f" = ".git" ] && continue
+  [ "$f" = ".config" ] && continue
+  ln -snfv $DOTPATH/"$f" ~/
 done
 
-ln -snfv $DOTPATH/.config/dunst ~/.config/
-ln -snfv $DOTPATH/.config/i3 ~/.config/
-ln -snfv $DOTPATH/.config/picom ~/.config/
-ln -snfv $DOTPATH/.config/rofi ~/.config/
+ln -snfv $DOTPATH/.config/nvim ~/.config/
 
+if [ "$(uname)" == 'Darwin' ]; then
+
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  ln -snfv $DOTPATH/.config/dunst ~/.config/
+  ln -snfv $DOTPATH/.config/i3 ~/.config/
+  ln -snfv $DOTPATH/.config/picom ~/.config/
+  ln -snfv $DOTPATH/.config/rofi ~/.config/
+else
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
+fi
